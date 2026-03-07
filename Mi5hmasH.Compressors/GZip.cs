68bd669ea84d21755adec21d3015d9ae0180f655
Michał Gębicki <59212335+mi5hmash@users.noreply.github.com>
2026-03-07 18:36:25 +0100

    Refactor GZip helpers to instance-style extensions

--- Mi5hmasH.Compressors/GZip.cs
+++ Mi5hmasH.Compressors/GZip.cs
@@ -7,67 +7,73 @@ namespace Mi5hmasH.Compressors;
 /// </summary>
 public static class GZip
 {
-    /// <summary>
-    /// Compresses a byte array using GZip compression.
-    /// </summary>
     /// <param name="bytes">The data to compress as a byte array.</param>
-    /// <param name="compressionLevel">The level of compression to apply, ranging from NoCompression to Optimal.</param>
-    /// <returns>A byte array containing the compressed data.</returns>
-    public static byte[] GzipCompress(this byte[] bytes, CompressionLevel compressionLevel = CompressionLevel.Optimal)
+    extension(byte[] bytes)
     {
-        using var mso = new MemoryStream();
-        using (var gz = new GZipStream(mso, compressionLevel))
-        {
-            gz.Write(bytes, 0, bytes.Length);
-        }
-        return mso.ToArray();
-    }
+        /// <summary>
+        /// Determines whether the underlying byte array represents data in the Gzip compressed format.
+        /// </summary>
+        /// <returns><see langword="true"/> if the byte array begins with the Gzip magic number; otherwise, <see langword="false"/>.</returns>
+        public bool IsGzip() => bytes is [0x1F, 0x8B, ..];
 
-    /// <summary>
-    /// Asynchronously compresses a byte array using GZip compression.
-    /// </summary>
-    /// <param name="bytes">The data to compress as a byte array.</param>
-    /// <param name="compressionLevel">The level of compression to apply, ranging from NoCompression to Optimal.</param>
-    /// <returns>A task that represents the asynchronous operation. The task result is a byte array containing the compressed data.</returns>
-    public static async Task<byte[]> GZipCompressAsync(this byte[] bytes, CompressionLevel compressionLevel = CompressionLevel.Optimal)
-    {
-        using var mso = new MemoryStream();
-        await using (GZipStream gz = new(mso, compressionLevel))
+        /// <summary>
+        /// Compresses a byte array using GZip compression.
+        /// </summary>
+        /// <param name="compressionLevel">The level of compression to apply, ranging from NoCompression to Optimal.</param>
+        /// <returns>A byte array containing the compressed data.</returns>
+        public byte[] GzipCompress(CompressionLevel compressionLevel = CompressionLevel.Optimal)
         {
-            gz.Write(bytes, 0, bytes.Length);
+            using var mso = new MemoryStream();
+            using (var gz = new GZipStream(mso, compressionLevel))
+            {
+                gz.Write(bytes, 0, bytes.Length);
+            }
+            return mso.ToArray();
         }
-        return mso.ToArray();
-    }
 
-    /// <summary>
-    /// Decompresses a byte array that was compressed using GZip compression.
-    /// </summary>
-    /// <param name="bytes">The GZip-compressed data as a byte array.</param>
-    /// <returns>A byte array containing the decompressed data.</returns>
-    public static byte[] GzipDecompress(this byte[] bytes)
-    {
-        using var msi = new MemoryStream(bytes);
-        using var mso = new MemoryStream();
-        using (var gz = new GZipStream(msi, CompressionMode.Decompress))
+        /// <summary>
+        /// Asynchronously compresses a byte array using GZip compression.
+        /// </summary>
+        /// <param name="compressionLevel">The level of compression to apply, ranging from NoCompression to Optimal.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is a byte array containing the compressed data.</returns>
+        public async Task<byte[]> GZipCompressAsync(CompressionLevel compressionLevel = CompressionLevel.Optimal)
         {
-            gz.CopyTo(mso);
+            using var mso = new MemoryStream();
+            await using (GZipStream gz = new(mso, compressionLevel))
+            {
+                gz.Write(bytes, 0, bytes.Length);
+            }
+            return mso.ToArray();
         }
-        return mso.ToArray();
-    }
 
-    /// <summary>
-    /// Asynchronously decompresses a byte array that was compressed using GZip compression.
-    /// </summary>
-    /// <param name="bytes">The GZip-compressed data as a byte array.</param>
-    /// <returns>A task that represents the asynchronous operation. The task result is a byte array containing the decompressed data.</returns>
-    public static async Task<byte[]> GZipDecompressAsync(this byte[] bytes)
-    {
-        using var msi = new MemoryStream(bytes);
-        using var mso = new MemoryStream();
-        await using (var gz = new GZipStream(msi, CompressionMode.Decompress))
+        /// <summary>
+        /// Decompresses a byte array that was compressed using GZip compression.
+        /// </summary>
+        /// <returns>A byte array containing the decompressed data.</returns>
+        public byte[] GzipDecompress()
         {
-            await gz.CopyToAsync(mso);
+            using var msi = new MemoryStream(bytes);
+            using var mso = new MemoryStream();
+            using (var gz = new GZipStream(msi, CompressionMode.Decompress))
+            {
+                gz.CopyTo(mso);
+            }
+            return mso.ToArray();
         }
-        return mso.ToArray();
+
+        /// <summary>
+        /// Asynchronously decompresses a byte array that was compressed using GZip compression.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result is a byte array containing the decompressed data.</returns>
+        public async Task<byte[]> GZipDecompressAsync()
+        {
+            using var msi = new MemoryStream(bytes);
+            using var mso = new MemoryStream();
+            await using (var gz = new GZipStream(msi, CompressionMode.Decompress))
+            {
+                await gz.CopyToAsync(mso);
+            }
+            return mso.ToArray();
+        }
     }
 }
